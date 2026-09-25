a593787f9f3ca54da962cae493d09cf26261d2b6
ybak4987-creator <ybak4987@gmail.com>
2026-09-25 14:37:48 +0300

    Delete src directory

--- src/constants/brand.ts
+++
@@ -1,7 +0,0 @@
-export const BRAND = {
-  name: 'TIKTOK KURA',
-  username: '@bilgi_xwrtxwrt',
-  profileUrl: 'https://www.tiktok.com/@bilgi_xwrtxwrt',
-  logoUrl: '/logo.svg',
-  fallbackLogoUrl: '/logo.svg',
-};
